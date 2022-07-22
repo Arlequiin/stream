6aa4a2abe14b930ccf278a429df2b8622f9f6552
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2022-07-22 16:22:10 +0200

    au bon endroit

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk # Python 3
+root = tk.Tk()
+root.image = tk.PhotoImage(file='chibi.gif')
+label = tk.Label(root, image=root.image, bg='white')
+root.overrideredirect(True)
+root.geometry("+1100+572")
+root.lift()
+root.wm_attributes("-topmost", True)
+root.wm_attributes("-disabled", True)
+root.wm_attributes("-transparentcolor", "white")
+label.pack()
+label.mainloop()
